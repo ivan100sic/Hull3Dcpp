42ffa735427eb74a6313f797edf963d670384894
ivan100sic <ivan100sic@gmail.com>
2020-07-15 17:48:50 +0200

    Initial commit

---
+++ ConvexHull3D/point.h
@@ -0,0 +1,26 @@
+#pragma once
+
+template<class T>
+struct point {
+	T x, y, z;
+
+	point operator- (const point& b) const {
+		return { x - b.x, y - b.y, z - b.z };
+	}
+
+	point& operator-= (const point& b) {
+		return *this = *this - b;
+	}
+};
+
+template<class T>
+T orientation(const point<T>& p, const point<T>& q, const point<T>& r, const point<T>& s) {
+	T sum {};
+	point<T> a = q - p;
+	point<T> b = r - p;
+	point<T> c = s - p;
+	sum += a.x * (b.y * c.z - b.z * c.y);
+	sum += a.y * (b.z * c.x - b.x * c.z);
+	sum += a.z * (b.x * c.y - b.y * c.x);
+	return sum;
+}
